b65b706208d55d8f3ec776ea17343e085660bc27
Ethan Wang <120232327046@ncepu.edu.cn>
2023-12-25 16:17:33 +0000

    Update image alt text and change heading colors

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -23,7 +23,8 @@ import PropTypes from "prop-types";
 //import image from "../images/woman-with-tablet.jpg";
 import image from "../images/ethanwang-use-laptop.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+//const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "Adult male in office setting using a laptop computer";
 
 const Home = ({ name, title }) => {
   return (
